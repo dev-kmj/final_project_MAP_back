860b8b27fc73c44febe62893d602b297ef4cba81
Yoon321 <tkdwls734@gmail.com>
2022-07-08 12:01:13 +0900

    fix: cors 정책 수정

--- finalProjectBack/src/main/java/com/sparta/finalprojectback/filter/CorsFilter.java
+++ finalProjectBack/src/main/java/com/sparta/finalprojectback/filter/CorsFilter.java
@@ -22,7 +22,7 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8090");
+        response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500/");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","*");
         response.setHeader("Access-Control-Max-Age", "3600");
