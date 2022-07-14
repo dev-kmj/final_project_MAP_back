fed307ec946ca5678c9d14d9dc9cfe8d18ed8e84
moonjoeng <dev.moonjeong@gmail.com>
2022-07-14 23:01:38 +0900

    feat : 카카오 로그인

---
+++ finalProjectBack/src/main/java/com/sparta/finalprojectback/kakao/KakaoLoginController.java
@@ -0,0 +1,33 @@
+package com.sparta.finalprojectback.kakao;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class KakaoLoginController {
+
+    private final KakaoLoginService kakaoLoginService;
+
+
+    // 프론트에서 인가코드를 받아와 토큰 생성
+    // 일단 print로 확인 완료 (코드, 토큰 둘 다 출력)
+    @GetMapping("/auth/kakao/callback")
+    public void kakaoCallback(@RequestParam String code) {
+//        System.out.println("Authorized code : " + code);
+//        String accessToken = kakaoOAuth.getAccessToken(code);
+//        System.out.println("accessToken: " + accessToken);
+
+        kakaoLoginService.kakaoLogin(code);
+    }
+
+
+    // 진행중
+//    @PostMapping("/logout/kakao")
+//    public void kakaoLogout(@RequestParam ) {
+//
+//    }
+
+}
