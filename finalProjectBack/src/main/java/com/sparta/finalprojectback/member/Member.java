58ccb7470a1ab1f659f0d8c47f7d2a3fe885dc22
eoeo777755@naver.com <eoeo777755@naver.com>
2022-07-19 12:34:06 +0900

    feat : unique 제거

--- finalProjectBack/src/main/java/com/sparta/finalprojectback/member/Member.java
+++ finalProjectBack/src/main/java/com/sparta/finalprojectback/member/Member.java
@@ -25,13 +25,13 @@ public class Member extends Timestamped implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 100, nullable = false, unique = true)
+    @Column(length = 100, nullable = false)
     private String username;
 
     @Column(length = 300, nullable = false)
     private String password;
 
-    @Column(length = 20, nullable = false, unique = true)
+    @Column(length = 20, nullable = false)
     private String nickname;
 
     @Column(length = 100, nullable = false)
