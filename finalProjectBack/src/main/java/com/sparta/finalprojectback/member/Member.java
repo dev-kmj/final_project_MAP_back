8d5daa50a3d1982331318a8637fe188d9ac0ecbd
Yoon321 <tkdwls734@gmail.com>
2022-07-21 17:13:18 +0900

    test: 깃 액션 테스트

--- finalProjectBack/src/main/java/com/sparta/finalprojectback/member/Member.java
+++ finalProjectBack/src/main/java/com/sparta/finalprojectback/member/Member.java
@@ -27,7 +27,7 @@ public class Member extends Timestamped implements UserDetails {
     private Long id;
 
     @Column(length = 100, nullable = false, unique = true)
-    private String username;
+    private String username; 
 
     @Column(length = 300, nullable = false)
     private String password;
