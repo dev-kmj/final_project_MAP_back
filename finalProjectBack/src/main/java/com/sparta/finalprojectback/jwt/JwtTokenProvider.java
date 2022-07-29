462c1d3b09eaad28bb437766903b283237ed92be
Yoon321 <tkdwls734@gmail.com>
2022-07-29 11:36:40 +0900

    refactor: 시크릿 키 숨김

--- finalProjectBack/src/main/java/com/sparta/finalprojectback/jwt/JwtTokenProvider.java
+++ finalProjectBack/src/main/java/com/sparta/finalprojectback/jwt/JwtTokenProvider.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,7 +22,8 @@ import java.util.List;
 @Component
 public class JwtTokenProvider {
 
-    private String secretKey = "dflaneiollsgd";
+    @Value("${secretKey}")
+    private String secretKey;
 
     //토큰 유효시간 30분 * 4 으로 설정
     private long tokenValidTime = 30 * 60 * 1000L * 4;
